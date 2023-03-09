66b176da83a6931c8831f5a2393dd59dd197e6b0
Thomas Wilson <tommyhwilson3@gmail.com>
2023-03-08 19:07:13 -0600

    refactored and completed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 # STORIES = {
-#     "silly_story": {
+#     "excited_story": {
 #         "promtps": ["place", "noun", "verb", "adjective", "plural_noun"],
 #         "story": """Once upon a time, in a long-ago {place}, there lived an exceptionally
 #        {adjective} {noun}. It loved to {verb} with {plural_noun}."""
@@ -23,8 +23,10 @@ debug = DebugToolbarExtension(app)
 
 @app.get("/")
 def generate_prompt_form():
-    prompts = silly_story.prompts
+    prompts = excited_story.prompts
     return render_template('questions.html', prompts=prompts )
 
-# @app.get("/results")
-# def
+@app.get("/results")
+def generate_story():
+    answers = request.args
+    return render_template('results.html', results = excited_story.generate(answers))
