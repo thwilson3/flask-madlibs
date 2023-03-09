f7eb2a66f79612d82fe4c7dac22eaf54634eec03
Evan Hesketh <evanhesketh@yahoo.ca>
2023-03-08 19:42:57 -0500

    add generate_prompt_form and update results.html

--- app.py
+++ app.py
@@ -7,3 +7,24 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+# STORIES = {
+#     "silly_story": {
+#         "promtps": ["place", "noun", "verb", "adjective", "plural_noun"],
+#         "story": """Once upon a time, in a long-ago {place}, there lived an exceptionally
+#        {adjective} {noun}. It loved to {verb} with {plural_noun}."""
+#     },
+
+#     "excited_story": {
+#         "prompts": ["noun", "verb"],
+#         "story": """OMG!! OMG!! I love to {verb} a {noun}!"""
+#     }
+# }
+
+@app.get("/")
+def generate_prompt_form():
+    prompts = silly_story.prompts
+    return render_template('questions.html', prompts=prompts )
+
+# @app.get("/results")
+# def
